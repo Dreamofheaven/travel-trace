24d93cb90d9781ea71808cf12d946e7b97912456
Hanui Jang <hany0147@naver.com>
2023-05-02 15:27:31 +0900

    [UPDATE] Social login3

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -10,10 +10,20 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('', views.UserView.as_view()),
 
-    # 소셜 로그인
-    # path('google/login/', views.google_login, name='google_login'),
-    # path('google/callback/', views.google_callback, name='google_callback'),
+    # 구글
+    path('google/login/', views.google_login, name='google_login'),
+    path('google/callback/', views.google_callback, name='google_callback'),
     path('google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
+
+    # 카카오
+    path('kakao/login/', views.kakao_login, name='kakao_login'),
+    path('kakao/callback/', views.kakao_callback, name='kakao_callback'),
+    path('kakao/login/finish/', views.KakaoLogin.as_view(), name='kakao_login_todjango'),
+
+    # 네이버
+    path('naver/login', views.naver_login, name='naver_login'),
+    path('naver/callback/', views.naver_callback, name='naver_callback'),
+    path('naver/login/finish/', views.NaverLogin.as_view(), name='naver_login_todjango'),
 
     # 프로필 및 북마크
     path('profile/<int:user_pk>/', views.ProfileView.as_view()),
