44f074a642607972d1c6f58a634fcac99a41e5a4
DreamofHeaven <eunjeong0223@naver.com>
2023-05-04 20:40:27 +0900

    [DELETE]불필요한 파일 삭제

--- frontend/traveltraceprj/src/index.js
+++ frontend/traveltraceprj/src/index.js
@@ -4,19 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>,
+      <App />
     </BrowserRouter>
   </React.StrictMode>
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
