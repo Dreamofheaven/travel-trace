c506e4161779a33c0dc010a59aa0fd1860da7762
Hanui Jang <hany0147@naver.com>
2023-05-05 11:03:50 +0900

    [UPDATE] article posting fin

--- frontend/traveltraceprj/src/components/Images.js
+++ frontend/traveltraceprj/src/components/Images.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
-import { PlusCircleFill, X} from 'react-bootstrap-icons'
+import { PlusCircleFill, Trash3, Images} from 'react-bootstrap-icons'
 import "../styles/Images.css";
 
-function Images() {
+function ImageFuntion() {
   const [showImages, setShowImages] = useState([]);
 
   // 이미지 상대경로 저장
@@ -32,17 +32,19 @@ function Images() {
     <div className='add_picture'>
       <label htmlFor="input-file" className='add_btn' type="button" onChange={handleAddImages}>
         <input type="file" className='add_btn2' id="input-file" multiple />
-        <PlusCircleFill className='text-self-center' fill="black" />
-        <span className='ms-2'>사진추가</span>
+        <span className='ms-2'><Images className='images_icon' /></span>
+        {/* <PlusCircleFill className='text-self-center ms-2 pb-1' fill="black" /> */}
       </label>
-      {/* // 저장해둔 이미지들을 순회하면서 화면에 이미지 출력 */}
-      {showImages.map((image, id) => (
-        <div className='img_container' key={id}>
-          <img src={image} className='trip_img' alt={`${image}-${id}`} />
-          <X className='x_icon' onClick={() => handleDeleteImage(id)} />
-        </div>
-      ))}
+      {/* 저장해둔 이미지들을 순회하면서 화면에 이미지 출력 */}
+      <div className='order_picture'>
+        {showImages.map((image, id) => (
+          <div className='img_container' key={id}>
+            <img src={image} className='trip_img' alt={`${image}-${id}`} />
+            <Trash3 className='trash_icon' onClick={() => handleDeleteImage(id)} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-export default Images;
+export default ImageFuntion;
