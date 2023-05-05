4ff8e70afbeb3d1f169ab6422e0d62b39ef68ccd
DreamofHeaven <eunjeong0223@naver.com>
2023-05-05 17:17:16 +0900

    [FIX]내브바에 로그아웃 함수기능

--- frontend/traveltraceprj/src/pages/All.js
+++ frontend/traveltraceprj/src/pages/All.js
@@ -7,13 +7,12 @@ function All() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
+    const token = localStorage.getItem('access');
+    console.log(token)
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
     const fetchData = async () => {
-      const token = localStorage.getItem('access_token'); // 로컬 스토리지에서 인증 토큰 가져오기
-      const result = await axios.get('http://127.0.0.1:8000/articles/', {
-        headers: {
-          Authorization: 'Bearer ${token}' 
-        }
-      });
+      const result = await axios.get('http://127.0.0.1:8000/articles/');
       setArticles(result.data);
     };
 
@@ -35,3 +34,41 @@ function All() {
 }
 
 export default All;
+
+
+
+
+/////////////////////////////////
+// function All() {
+//   const [articles, setArticles] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const token = localStorage.getItem('access'); // 로컬 스토리지에서 인증 토큰 가져오기
+//       console.log(token)
+//       const result = await axios.get('http://127.0.0.1:8000/articles/', {
+//         headers: {
+//           Authorization: `Bearer ${token}`
+//         }
+//       });
+//       setArticles(result.data);
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div>
+//       {articles.map(article => (
+//         <div key={article.id}>
+//           <h2>{article.title}</h2>
+//           <h2>{article.user}</h2>
+//           <h2>{article.image}</h2>
+//           <img src={article.image} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default All;
