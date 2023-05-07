6f2189c1f12858aa6e012f8490f5bd8086c99cc7
meni2000 <meni2000@naver.com>
2023-05-07 23:15:32 +0900

    [UPDATE] Detail_comment_like

---
+++ frontend/traveltraceprj/src/components/CommentLikeBtn.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { HandThumbsUp, HandThumbsUpFill } from 'react-bootstrap-icons';
+
+function CommentLikeBtn({ commentId }) {
+  const [liked, setLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  const handleLike = () => {
+    if (liked) {
+      // Perform unlike logic
+      setLiked(false);
+      setLikeCount(likeCount - 1);
+    } else {
+      // Perform like logic
+      setLiked(true);
+      setLikeCount(likeCount + 1);
+    }
+  };
+
+  return (
+    <button
+      className="btn btn-success ms-auto"
+      style={{ backgroundColor: 'transparent', border: 'none' }}
+      onClick={handleLike}
+    >
+      {liked ? (
+        <HandThumbsUpFill size={20} style={{ color: '#A0D468' }} />
+      ) : (
+        <HandThumbsUp size={20} style={{ color: '#A0D468' }} />
+      )}
+    </button>
+  );
+}
+
+export default CommentLikeBtn;
